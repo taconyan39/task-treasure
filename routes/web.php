96a6755279508344291e2e7ee8814b715eeb64f3
taconyan39 <taconyan39@gmail.com>
2026-07-08 06:42:39 +0900

    web.php修正

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Artisan;
 
-// トップページ（http://127.0.0.1:8000）にアクセスした時に、あのRPG風画面を表示させるわ！
+// トップページ（http://127.0.0.1:8000）にアクセスした時に、あのRPG風画面を表示させる
 Route::get('/', function () {
     return view('task_treasure');
 });
-
-use App\Http\Controllers\AuthController;
 
 // 新規登録の画面と処理
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
@@ -17,13 +17,11 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
-// ログアウトの処理
-out'])->name('logout');Route::post('/logout', [AuthController::class, 'log
+// ログアウトの処理（ぐちゃぐちゃになっていた部分を修正！）
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-use Illuminate\Support\Facades\Artisan;
-
-// マイグレーションを実行するための特別なURLなの
+// マイグレーションを実行するための特別なURL
 Route::get('/migrate-db', function () {
     Artisan::call('migrate', ['--force' => true]);
     return 'オーッホッホッホ♪ マイグレーション大成功なの！';
-});
+}); // ← このカッコでしっかり閉じるの！
